1c9edc8013723b23414263101420dd3632d2e53b
tsukuneA1 <tkschr1125@gmail.com>
2025-09-13 23:27:04 +0900

    fmt

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import {
-	Card,
-	CardContent,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { RotatableAvatar } from "@/components/ui/rotatable-avatar";
 import { Typewriter } from "@/components/ui/typewriter";
